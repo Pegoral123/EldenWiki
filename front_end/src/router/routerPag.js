72d50ffa3022f7605604fa7c4716896a2db93181
Pegoral123 <jhonathanpegoral123@gmail.com>
2026-08-11 19:17:42 -0300

    refactor: adiciona nomes às rotas no arquivo routerPag.js

--- front_end/src/router/routerPag.js
+++ front_end/src/router/routerPag.js
@@ -1,18 +1,23 @@
 const routes = [
   {
     path: "/",
+    name: "home",
     component: () => import("@/components/homePage.vue"),
   },
   {
     path: "/mainPage",
+    name: "mainPage",
     component: () => import("@/components/mainPage.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/LoginPage",
+    name: "login",
     component: () => import("@/components/LoginPage.vue"),
   },
   {
     path: "/RegisterPage",
+    name: "register",
     component: () => import("@/components/RegisterPage.vue"),
   },
 ];
