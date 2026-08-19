1a706e5807a2e389d404f82c02fc3e8318fc3557
Pegoral123 <jhonathanpegoral123@gmail.com>
2026-08-19 19:51:11 -0300

    feat: adiciona configuração inicial do FastAPI com rotas e CORS

---
+++ back_end/app/main.py
@@ -0,0 +1,36 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+from app.api.routes import auth, bosses, locations
+
+BACKEND_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BACKEND_DIR, "static")
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
+app.include_router(locations.router)
+app.include_router(bosses.router)
+app.include_router(auth.router)
+
+origins = [
+    "http://localhost:5173",  # A origem do app Vue
+    "http://127.0.0.1:5173",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*", "Authorization"],
+)
+
+
+@app.get("/")
+def raiz_root():
+    return {"message": "Bem-vindo à ELDEN WIKI!"}
